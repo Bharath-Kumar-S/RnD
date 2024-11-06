d0bd48d99cc2a2012e6e451c27629669b56ee04f
Bharath Kumar <bharathkumarkarthick@gmail.com>
2024-11-06 22:40:56 +0200

    Add mongodb

--- backend/src/controller/priceController/priceController.ts
+++ backend/src/controller/priceController/priceController.ts
@@ -1,5 +1,6 @@
 import { Response, Request } from "express";
 import * as dotenv from "dotenv";
+import { PriceHistory } from "../../models/priceHistory"; // Adjust path as necessary
 dotenv.config();
 
 export const getPrice = async (req: Request, res: Response) => {
@@ -12,7 +13,6 @@ export const getPrice = async (req: Request, res: Response) => {
   const pricingServiceUrl = `${process.env.PRICING_SERVICE_API}?symbol=${String(
     symbol
   ).replace(/\//g, "")}`;
-  console.log(req.query.symbol);
 
   try {
     const response = await fetch(pricingServiceUrl);
@@ -28,6 +28,15 @@ export const getPrice = async (req: Request, res: Response) => {
       [String(symbol).replace(/\//g, "")]: tonUsdtPrice,
       [String(symbol).split("/").reverse().join("")]: 1 / tonUsdtPrice,
     };
+
+    // Save the price data to the database
+    const newPriceHistory = new PriceHistory({
+      pair: String(symbol).replace(/\//g, ""),
+      price: tonUsdtPrice,
+      inversePrice: 1 / tonUsdtPrice,
+    });
+
+    await newPriceHistory.save();
 
     return res.status(200).send(prices);
   } catch (error) {
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { redisClient } from "./service/redis";
 import * as dotenv from "dotenv";
 import { priceRouter } from "./router/priceRouter";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -22,5 +23,18 @@ app.listen(PORT, () => {
   );
   if (redisClient.isOpen) {
     console.log("Redis client is connected");
+  }
+  try {
+    console.log("app is started");
+    if (!(process.env.NODE_ENV === "test")) {
+      mongoose.connect(`${process.env.DBSTRING}`);
+      const db = mongoose.connection;
+      db.on("error", (err) => {
+        console.error(err);
+      });
+      db.on("open", () => console.log("Connected to DB!!!!"));
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
