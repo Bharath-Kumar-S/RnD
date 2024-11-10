5f36075b3b3aad6fc1fec14058b0fcc1f170fb77
Bharath Kumar <bharathkumarkarthick@gmail.com>
2024-11-10 13:29:06 +0200

    Adding react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { settings } from "./settings";
 import { PriceCard } from "./components/PriceCard/PriceCard";
+import { useQuery } from "@tanstack/react-query";
+
+const getPrice = async (currency: string) => {
+  const response = await fetch(
+    `${settings.api.baseUrl}/price?symbol=${currency}`
+  );
+  const data = await response.json();
+  return data;
+};
 
 function App() {
   const [currency, setCurrency] = useState("TON/USDT");
-  const [prices, setPrices] = useState();
-  const [invertedPrices, setInvertedPrices] = useState();
 
-  useEffect(() => {
-    fetch(`${settings.api.baseUrl}/price?symbol=${currency}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setPrices(data[currency.replace("/", "")]);
-        setInvertedPrices(data[currency.split("/").reverse().join("")]);
-      })
-      .catch((error) => console.error("Error fetching prices:", error));
-  }, [currency]);
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["price", currency],
+    queryFn: () => getPrice(currency),
+    staleTime: 1800000, // 30 minutes cache for API response
+  });
+
+  const prices = data ? data[currency.replace("/", "")] : null;
+  const invertedPrices = data
+    ? data[currency.split("/").reverse().join("")]
+    : null;
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-900 via-blue-800 to-blue-900">
@@ -38,6 +46,10 @@ function App() {
       </div>
 
       <div className="grid md:grid-cols-2 gap-8 max-w-3xl mx-auto my-10">
+        {isLoading && <p className="text-center text-white">Loading...</p>}
+        {isError && (
+          <p className="text-center text-red-500">Error fetching data.</p>
+        )}
         {prices && <PriceCard currency="TON/USDT" price={prices} />}
         {invertedPrices && (
           <PriceCard currency="USDT/TON" price={invertedPrices} />
